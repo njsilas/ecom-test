a2f71e30c08df75eb2e1def2750f12497df95f2d
Nicholas Silas <njsilas@gmail.com>
2021-08-09 15:49:38 -0400

    updating from main

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Client from 'shopify-buy'
 import { BrowserRouter as Router } from 'react-router-dom'
-
+const client = Client.buildClient({
+  storefrontAccessToken: '775f212258a0b29478371a6edbdab2d5',
+  domain: 'ecom-mock-up.myshopify.com'
+  })
 
 ReactDOM.render(
   <Router>
-    <App />
+    <App client={client}/>
     </Router>,
 document.getElementById('root')
 );
 
-const client = Client.buildClient({
-  storefrontAccessToken: '775f212258a0b29478371a6edbdab2d5',
-  domain: 'ecom-mock-up.myshopify.com'
-  });
+;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
